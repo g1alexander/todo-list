0caaad76cc21edfc8af01cdb1df60537c1651cf3
g1alexander <alexlds26@gmail.com>
2022-02-04 21:01:51 -0500

    feat: plugin test of pwa

--- src/modules/todo/view/TodoView.vue
+++ src/modules/todo/view/TodoView.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
 import Todo from "../components/Todo.vue";
-import TestVue from "../components/Test.vue";
 </script>
 
 <template>
   <main class="text-3xl font-bold underline">
     <Todo />
-    <TestVue />
   </main>
 </template>
