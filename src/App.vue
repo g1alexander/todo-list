0fea60972c0c14f6de93980774bb2d8108eb1446
g1alexander <alexlds26@gmail.com>
2022-02-04 20:48:06 -0500

    feat: plugin test of pwa

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
+import ReloadVue from "./global/components/Reload.vue";
 </script>
 
 <template>
+  <ReloadVue />
   <RouterView />
 </template>
 
